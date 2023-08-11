ffd7e38da2ccb58077c0dea60fa43d52a53da533
Sujan Paudel <sujan2000paudel@gmail.com>
2023-08-11 16:30:08 +0545

    solve importing csv

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,77 @@
-// import { useEffect, useState } from "react";
-// import Papa from 'papaparse';
-// import Container from "@mui/material/Container";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
+import Container from "@mui/material/Container";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-// import Header from "./components/Header";
+import Header from "./components/Header";
 
 // import csvData from "./assets/TrackedTime.csv";
 
-// const App = () => {
-//   const [csvData, setCsvData] = useState([]);
+const App = () => {
+  // const [csvData, setCsvData] = useState([]);
 
-//   useEffect(() => {
-//     Papa.parse(csvData, {
-//       header: true,
-//       skipEmptyLines: true,
-//       complete: (result) => {
-//         setData(result.data);
-//       },
-//       error: (error) => {
-//         console.error("Error parsing CSV:", error.message);
-//       },
-//     });
-//   }, []);
-
-//   const importCSV = () => {
-//     console.log("importCSV");
-//   };
-
-//   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <Container>
-//         <Header clickImport={importCSV} />
-//       </Container>
-//     </LocalizationProvider>
-//   );
-// };
-
-// export default App;
-
-// // <DatePicker value={value} onChange={(newValue) => setValue(newValue)} label="Start Date" />
-
-import React, { useState, useEffect } from "react";
-import Papa from "papaparse";
-import csvData from "../src/assets/TrackedTime.csv";
-
-function App() {
-  const [data, setData] = useState([]);
-  // console.log(csvData)
+  // useEffect(() => {
+  //   Papa.parse("../src/assets/TrackedTime.csv", {
+  //     header: true,
+  //     skipEmptyLines: true,
+  //     complete: (result) => {
+  //       console.log(result.data)
+  //     },
+  //     error: (error) => {
+  //       console.error("Error parsing CSV:", error.message);
+  //     },
+  //   });
+  // }, []);
 
   useEffect(() => {
-    // Papa.parse(csvData, {
-    //   header: true,
-    //   skipEmptyLines: true,
-    //   complete: (result) => {
-    //     console.log(result);
-    //     // setData(result.data)
-    //   },
-    //   error: (error) => {
-    //     console.error('Error parsing CSV:', error.message);
-    //   },
-    // });
-    Papa.parse("./assets/TrackedTime.csv", {
-      // download: true,
-      // header: true,
+    Papa.parse("../src/assets/TrackedTime.csv", {
+      download: true,
       header: true,
-      delimiter: ",",
+      skipEmptyLines: true,
       complete: (data) => {
         console.log(data.data);
+        // setRows(data.data);
+      },
+      error: (error) => {
+        console.error("Error parsing CSV:", error.message);
       },
     });
   }, []);
 
+  const importCSV = () => {
+    console.log("importCSV");
+  };
+
   return (
-    <div>
-      <h1>CSV Data</h1>
-      <ul>
-        {data.map((entry, index) => (
-          <li key={index}>{JSON.stringify(entry)}</li>
-        ))}
-      </ul>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Container>
+        <Header clickImport={importCSV} />
+      </Container>
+    </LocalizationProvider>
   );
-}
+};
 
 export default App;
+
+// // <DatePicker value={value} onChange={(newValue) => setValue(newValue)} label="Start Date" />
+
+// import React, { useState, useEffect } from "react";
+// import Papa from "papaparse";
+
+// function App() {
+//   const [rows, setRows] = React.useState(null);
+
+//   return (
+//     <div>
+//       <h1>CSV Data</h1>
+//       {/* <ul>
+//         {data.map((entry, index) => (
+//           <li key={index}>{JSON.stringify(entry)}</li>
+//         ))}
+//       </ul> */}
+//     </div>
+//   );
+// }
+
+// export default App;
